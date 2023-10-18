6f82d7e30a390a6ee74d4523848cf87bf3713c7c
Yunish <yunish@gmail.com>
2023-10-18 07:30:22 +0545

    something chnaged

--- controller/adminController.js
+++ controller/adminController.js
@@ -37,7 +37,7 @@ module.exports.updateuserstatus=async(req,res)=>{
 module.exports.searchuserbyemail=async(req,res)=>{
     try {
       const {searchemail,email}=req.body;
-      let users=await User.find({searchemail});
+      let users=await User.find({email:searchemail});
       let filteredUsers = users.filter(user => user.email !== email);
       res.json(filteredUsers);
     } catch (e) {

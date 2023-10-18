74ea292417f324fd69522b84feb715b8f70911db
Yunish <yunish@gmail.com>
2023-10-18 10:08:13 +0545

    location added

--- controller/authController.js
+++ controller/authController.js
@@ -528,10 +528,14 @@ module.exports.deleteaccount=async(req,res)=>{
 }
 module.exports.updatelocation=async(req,res)=>{
   try {
-    const {email,lat,lng}=req.body;
+    const {email,lat,lng,country,state,city,location}=req.body;
     let user=await User.updateOne({email:email},{$set:{
       lat:lat,
-      lng:lng
+      lng:lng,
+      city:city,
+      state:state,
+      country:country,
+      location:location
     }});
     res.json(user);
   } catch (e) {

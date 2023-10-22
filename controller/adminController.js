a1281c385252d74cb06c6e6b96309e5b55ae11bb
Yunish <yunish@gmail.com>
2023-10-22 19:54:16 +0545

    something changed

--- controller/adminController.js
+++ controller/adminController.js
@@ -168,7 +168,8 @@ module.exports.searchuserbyemail=async(req,res)=>{
   }
   module.exports.searchusers=async(req,res)=>{
     try {
-        const {email,gender,page,ages,
+        const {email,
+          gender,page,ages,
            religionList,
           kundaliDoshList,
           maritalStatusList,
@@ -180,9 +181,18 @@ module.exports.searchuserbyemail=async(req,res)=>{
           educationList,
           professionList,
           incomeList,
+          longitude,
+          latitude,
+          maxDistanceKm,
           location}=req.body;
         const itemsPerPage = 100;
-        let users=await User.find({});   
+        if(maxDistanceKm){
+          const lon = parseFloat(longitude);
+          const lat = parseFloat(latitude);
+          let users=await User.find({$and:[
+            {  lng: { $gte: lon - (maxDistanceKm / 111.32), $lte: lon + (maxDistanceKm / 111.32) },},
+              {lat: { $gte: lat - (maxDistanceKm / (111.32 * Math.cos(lat * (Math.PI / 180)))), $lte: lat + (maxDistanceKm / (111.32 * Math.cos(lat * (Math.PI / 180)))) }}
+            ]});   
       
           if (!email) {
             return res.status(400).json({ error: 'Email is required in the request body.' });
@@ -270,6 +280,97 @@ filteredUsers.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
           res.json(
            paginatedUsers,
           );
+        }else{
+          let users=await User.find({});   
+      
+          if (!email) {
+            return res.status(400).json({ error: 'Email is required in the request body.' });
+          }
+        
+          if (!gender) {
+            return res.status(400).json({ error: 'Gender or religion is required in the request body.' });
+          }
+        
+          // Filter users based on gender and religion while excluding the user's own data
+          let filteredUsers = users.filter(user => user.email !== email);
+        // console.log(filteredUsers);
+          if (gender) {
+            filteredUsers = filteredUsers.filter(user => user.gender === gender);
+          }
+        
+          if(religionList.length){
+            filteredUsers = filteredUsers.filter(user => religionList.includes(user.religion) );
+
+         }
+          if(ages.length){
+            const intList = ages.map(str => parseInt(str));
+            console.log(intList);  
+            console.log(intList[0]);
+             filteredUsers = filteredUsers.filter(user => user.age >= intList[0] && user.age <= intList[1] );
+
+          }
+          if(kundaliDoshList.length){
+            console.log("ok")
+            filteredUsers = filteredUsers.filter(user => kundaliDoshList.includes(user.kundalidosh) );
+
+         }
+         if(maritalStatusList.length){
+          filteredUsers = filteredUsers.filter(user => maritalStatusList.includes(user.martialstatus) );
+
+       }
+       if(dietList.length){
+        filteredUsers = filteredUsers.filter(user => dietList.includes(user.diet) );
+
+     }
+     if(dietList.length){
+      filteredUsers = filteredUsers.filter(user => dietList.includes(user.diet) );
+
+   }
+   if(drinkList.length){
+    filteredUsers = filteredUsers.filter(user => drinkList.includes(user.drink) );
+
+ }
+ if(smokeList.length){
+  filteredUsers = filteredUsers.filter(user => smokeList.includes(user.smoke) );
+
+}
+if(disabilityList.length){
+  filteredUsers = filteredUsers.filter(user => disabilityList.includes(user.disability) );
+
+}
+if(heightList.length){
+  filteredUsers = filteredUsers.filter(user => heightList.includes(user.height) );
+
+}
+if(educationList.length){
+  filteredUsers = filteredUsers.filter(user => educationList.includes(user.education) );
+
+}
+if(professionList.length){
+  filteredUsers = filteredUsers.filter(user => professionList.includes(user.profession) );
+
+}
+if(incomeList.length){
+  filteredUsers = filteredUsers.filter(user => incomeList.includes(user.income) );
+
+}
+if(location.length){
+  filteredUsers = filteredUsers.filter(user => location.includes(user.location) );
+
+}
+filteredUsers.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+          console.log(filteredUsers);
+          // Paginate the results
+          const startIndex = (page - 1) * itemsPerPage;
+          const endIndex = startIndex + itemsPerPage;
+          const paginatedUsers = filteredUsers.slice(startIndex, endIndex);
+        
+          res.json(
+           paginatedUsers,
+          );
+        }
+       
     } catch (e) {
         res.status(500).json({mes:e.message})
     }
@@ -299,37 +400,16 @@ module.exports.getallnotification=async(req,res)=>{
 module.exports.searchuserbydistance=async(req,res)=>{
   try {
     const{longitude,latitude,maxDistanceKm,email}=req.body;
-    // const earthRadiusKm = 6371; // Approximate radius of the Earth in kilometers
-  // const maxDistanceRadians = maxDistanceKm / earthRadiusKm;
-  // const point = GeoJSON.Point([longitude, latitude]);
+  
   const lon = parseFloat(longitude);
   const lat = parseFloat(latitude);
-
-
-  
-
 
   const users = await User.find({$and:[
   {  lng: { $gte: lon - (maxDistanceKm / 111.32), $lte: lon + (maxDistanceKm / 111.32) },},
     {lat: { $gte: lat - (maxDistanceKm / (111.32 * Math.cos(lat * (Math.PI / 180)))), $lte: lat + (maxDistanceKm / (111.32 * Math.cos(lat * (Math.PI / 180)))) }}
   ]});
   let filteredUsers = users.filter(user => user.email !== email);
-//     const users = await User.find({
-//       $and: [
-//         {
-//           lng: {
-//             $gte: longitude - maxDistanceRadians, // Adjust the distance as needed
-//             $lte: longitude + maxDistanceRadians,
-//           },
-//         },
-//         {
-//           lat: {
-//             $gte: latitude - maxDistanceRadians, // Adjust the distance as needed
-//             $lte: latitude + maxDistanceRadians,
-//           },
-//         },
-//       ],
-//     });
+
 res.json(filteredUsers);
   } catch (e) {
     res.status(500).json({mes:e.message})

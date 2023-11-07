bad378119f8b18b680ed0f95d25eacd0597e5b96
Yunish <yunish@gmail.com>
2023-11-07 17:11:47 +0545

    something changed

--- controller/authController.js
+++ controller/authController.js
@@ -129,11 +129,11 @@ filteredUsers = filteredUsers.filter(user => location.includes(user.country) );
 
 }
 if(statelocation.length){
-filteredUsers = filteredUsers.filter(user => location.includes(user.state) );
+filteredUsers = filteredUsers.filter(user => statelocation.includes(user.state) );
 
 }
 if(citylocation.length){
-filteredUsers = filteredUsers.filter(user => location.includes(user.city) );
+filteredUsers = filteredUsers.filter(user => citylocation.includes(user.city) );
 
 }
 filteredUsers.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));

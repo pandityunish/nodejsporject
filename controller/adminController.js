df9e7b6c7a1142c79d60db9c06322419a2f621d3
Yunish <yunish@gmail.com>
2023-11-13 15:31:18 +0545

    something changed1

--- controller/adminController.js
+++ controller/adminController.js
@@ -480,7 +480,7 @@ module.exports.getallnotification = async (req, res) => {
 module.exports.searchnotification=async(req,res)=>{
   try {
     const {title}=req.body;
-    let noti=await AdminNotification.find({title:{ $regex: new RegExp(title, 'i') }});
+    let noti=await AdminNotification.find({subtitle:{ $regex: new RegExp(title, 'i') }});
     res.json(noti);
   } catch (e) {
     res.status(500).json({ message: e.message });

03f58d51543b64faee3b4356e3f0a1bcf9848094
Yunish <yunish@gmail.com>
2023-12-18 17:17:53 +0545

    something chnaged

--- controller/adminAddedController.js
+++ controller/adminAddedController.js
@@ -50,7 +50,7 @@ module.exports.updateallvalue=async(req,res)=>{
 module.exports.addtosendlink=async(req,res)=>{
     try {
       const {email,value}=req.body;
-      let user=await User.updateOne({email:email},{$addToSet:{
+      let user=await User.updateOne({email:email},{$push:{
         sendlink:value
       }});
     

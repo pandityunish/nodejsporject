562f4cacad011e66afee4951251e89ac31bbea0b
Yunish <yunish@gmail.com>
2024-03-07 10:34:05 +0545

    somethin

--- controller/queryController.js
+++ controller/queryController.js
@@ -1,3 +1,4 @@
+const Bubbles = require("../models/Bubbles");
 const Maintenance = require("../models/maintainance");
 const Query = require("../models/query_data");
 
@@ -29,6 +30,38 @@ module.exports.getMaintenance=async(req,res)=>{
         
         let query=await Maintenance.find({});
      
+      res.json(query);
+    } catch (e) {
+        res.status(500).json({mes:e.message})
+    }
+}
+module.exports.getBubbles=async(req,res)=>{
+    try {
+        
+        let query=await Bubbles.find({}).sort({createdAt:-1});
+    
+      res.json(query);
+    } catch (e) {
+        res.status(500).json({mes:e.message})
+    }
+}
+module.exports.createBubbles=async(req,res)=>{
+    try {
+        
+        let query=await Bubbles();
+    query=await query.save();
+      res.json(query);
+    } catch (e) {
+        res.status(500).json({mes:e.message})
+    }
+}
+module.exports.updateBubbles=async(req,res)=>{
+    try {
+        const {image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15,image16,image17,id}=req.body;
+        let query=await Bubbles.UpdateOne({_id:id},{$set:{
+           image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15,image16,image17
+        }});
+    
       res.json(query);
     } catch (e) {
         res.status(500).json({mes:e.message})
--- routers/queryRouter.js
+++ routers/queryRouter.js
@@ -1,10 +1,13 @@
 const express=require("express");
-const { postQuery, updateMaintenance, getMaintenance } = require("../controller/queryController");
+const { postQuery, updateMaintenance, getMaintenance, getBubbles, createBubbles, updateBubbles } = require("../controller/queryController");
 
  const queryRouter=express.Router();
 
 queryRouter.post("/postquery",postQuery);
 queryRouter.post("/updatemaintenance",updateMaintenance);
 queryRouter.get("/findmaintenance",getMaintenance);
+queryRouter.get("/getbubbles",getBubbles);
+queryRouter.get("/createbubbles",createBubbles);
+queryRouter.post("/updatebubbles",updateBubbles);
 
 module.exports=queryRouter;
